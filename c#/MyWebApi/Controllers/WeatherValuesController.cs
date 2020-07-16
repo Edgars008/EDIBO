b815176e03b4dec4088e972cc416e890da075397
Edgars008 <55804543+Edgars008@users.noreply.github.com>
2020-07-16 18:29:42 +0300

    Add files via upload

---
+++ c#/MyWebApi/Controllers/WeatherValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+           return new string[] {"Edgars", "2020", "EDIBO", "RTU"};
+        }
+    }
+}
