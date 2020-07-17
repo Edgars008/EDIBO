ceca071b1e30c62667378983b91c025ecaf39dcb
Edgars008 <55804543+Edgars008@users.noreply.github.com>
2020-07-17 10:03:05 +0300

    Update

--- 20200714/src.js
+++ 20200714/src.js
@@ -1,3 +1,34 @@
-fetch('https://www.bank.lv/vk/ecb.xml')
-  .then(response => response.json())
-  .then(data => console.log(data));
+const fetch = require("node-fetch");
+
+// fetch('./xml/books.xml')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// fetch('./xml/books.xml')
+// .then(function(resp) {
+// 	return resp.text();
+// })
+// .then(function(data) {
+// 	console.log(data);
+// 	let parser = new DOMParser(),
+// 	xmlDoc = parser.parserFromString(data, 'text/xml');
+// 		console.log(xmlDoc.getElementsByTagName('author'));
+// })
+
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
