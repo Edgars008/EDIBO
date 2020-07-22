7cc507a4345527b3436cf7bdbc0c7a6d0737e241
Edgars <edgarsnaglis300@gmail.com>
2020-07-22 17:38:57 +0300

    Update20200722

--- AngularTest/HelloWorld/src/app/app-routing.module.ts
+++ AngularTest/HelloWorld/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CurrencyModule} from './currency/currency.module';
+import {WeatherModule} from './weather/weather.module';
+import {AppComponent} from './app.component';
+import { CurrencyComponent } from './currency/currency.component';
+import { WeatherComponent } from './weather/weather.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent, pathMatch: 'full'},
+  {path: 'Currency', component: CurrencyComponent},
+  {path: 'Weather', component: WeatherComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularTest/HelloWorldLegacy/src/app/app.module.ts
+++ AngularTest/HelloWorldLegacy/src/app/app.module.ts
@@ -1,37 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
-
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card'; 
-import {MatCheckboxModule} from '@angular/material/checkbox'; 
-import {MatChipsModule} from '@angular/material/chips'; 
+import{CurrencyModule} from './currency/currency.module'
+import { AppRoutingModule } from './app-routing.module';
+import{WeatherModule} from './weather/weather.module'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatButtonModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    
+    CurrencyModule,
+    AppRoutingModule,
+    WeatherModule,
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
